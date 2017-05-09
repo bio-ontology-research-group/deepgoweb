954bccbcaeca0d9e5216eec7628a845089c4e29d
Maxat Kulmanov <coolmaksat@gmail.com>
2017-05-09 22:01:19 +0300

    .

--- deepgoweb/settings.py
+++ deepgoweb/settings.py
@@ -35,7 +35,7 @@ class BaseConfiguration(Configuration):
     # SECURITY WARNING: don't run with debug turned on in production!
     DEBUG = True
 
-    ALLOWED_HOSTS = []
+    ALLOWED_HOSTS = ['*']
 
     # Application definition
 
