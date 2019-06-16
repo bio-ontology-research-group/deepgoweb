a38eeb9bd4daf39f3bc6578424df0059b6b2085d
Maxat Kulmanov <coolmaksat@gmail.com>
2019-06-16 09:21:22 +0300

    celery workers

--- deepgoweb/settings/base.py
+++ deepgoweb/settings/base.py
@@ -168,8 +168,8 @@ CELERY_BROKER_URL = 'pyamqp://{user}:{pwd}@{host}:{port}//'.format(
     port=RABBIT_PORT)
 
 CELERY_RESULT_BACKEND = 'rpc://'
-CELERY_WORKER_CONCURRENCY = 1
-CELERY_BROKER_POOL_LIMIT = 1
+CELERY_WORKER_CONCURRENCY = 10
+CELERY_BROKER_POOL_LIMIT = 100
 CELERY_BROKER_CONNECTION_TIMEOUT = 10
 
 # configure queues, currently we have only one
